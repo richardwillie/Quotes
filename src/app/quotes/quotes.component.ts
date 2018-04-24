f7e093f1b3ef64adf88d5c3c5a9a035132e54e0f
richard willie <richardwillie.w@gmail.com>
2018-04-24 16:16:49 +0300

    modified inputquote form

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
   quote = 'God grant me the serenity to accept the things I cannot change, the courage to change the things I can, and the wisdom to know the difference.'
+  inputQuote = null;
   btnDisable = false;
   AddQuote(){
-    this.quote = "we must learn to live together as friends or die alone as fools"
+    this.quote = this.inputQuote
+    this.inputQuote = null 
   } 
 
   constructor() { }
